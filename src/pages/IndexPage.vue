7065eb92dae2a4fda9b77f5a1eeb122952de7651
Bruce Li <brucel@fleetprofitcenter.com>
2023-08-16 12:35:27 -0500

    added button vue component

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,11 +1,17 @@
 <template>
-  <q-page class="row items-center justify-evenly"> <div>123</div> </q-page>
+  <q-page class="row items-center justify-evenly">
+    <div>123</div>
+    <buttonVue></buttonVue
+  ></q-page>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue';
-
+import buttonVue from 'src/fpcUILibrary/components/example/button.vue';
 export default defineComponent({
   name: 'IndexPage',
+  components: {
+    buttonVue,
+  },
 });
 </script>
